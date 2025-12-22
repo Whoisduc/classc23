0413bd6f847c142139c71786d196d96a3f673a3e
andiko227 <nalendrasatriamajid@>
2025-12-22 21:57:30 +0700

    update

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ const members = [
   { name: "Repi Junita Sari", role: "Member", ig: "repijunitaaa", photo: "assets/members/repi.jpg" },
   { name: "Rima Ranataw", role: "Member", ig: "rimarnatw", photo: "assets/members/rima.jpg" },
   { name: "Rossa Florensia", role: "Member", ig: "rossaflorensia_", photo: "assets/members/rossa.jpg" },
-  { name: "Tohar", role: "Member", ig: "", photo: "assets/members/tohar.jpg" },
+  { name: "Tohar", role: "Member", ig: "tohar1305", photo: "assets/members/tohar.jpg" },
   { name: "Sultan Muliya Pratama", role: "Member", ig: "omsul._", photo: "assets/members/sultan.jpg" },
   { name: "Vira Dwi Novia", role: "Member", ig: "_viradwii", photo: "assets/members/vira.jpg" },
   { name: "Zahra Mutiara", role: "Member", ig: "zahra_mutiiara", photo: "assets/members/zahra.jpg" },
